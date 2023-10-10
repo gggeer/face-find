0954d89eabbe9beb8d19f07f79152e73f280317b
gggeer <143790130+gggeer@users.noreply.github.com>
2023-10-10 11:01:59 +0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,71 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
-
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+import tempfile
+import cv2
+import time
+import face_detection
+import numpy as np
 
 
-if __name__ == "__main__":
-    run()
+def extract_faces_from_image(im, bboxes):
+    extracted_faces = []
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))  # Adaptive Histogram Equalization
+
+    for bbox in bboxes:
+        x0, y0, x1, y1 = [int(_) for _ in bbox]
+        cropped_face = im[y0:y1, x0:x1]
+        cropped_face = cv2.resize(cropped_face, (cropped_face.shape[1] * 2, cropped_face.shape[0] * 2))
+
+        for channel in range(cropped_face.shape[2]):
+            cropped_face[:, :, channel] = clahe.apply(cropped_face[:, :, channel])
+
+        kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+        cropped_face = cv2.filter2D(cropped_face, -1, kernel)
+        extracted_faces.append(cropped_face)
+
+    return extracted_faces
+
+
+def process_video(video_file):
+    video_path = video_file.name
+    vidcap = cv2.VideoCapture(video_path)
+    detector = face_detection.build_detector("DSFDDetector", max_resolution=1080)
+
+    fps = int(vidcap.get(cv2.CAP_PROP_FPS))
+    all_faces = []
+    frame_count = 0
+    while True:
+        vidcap.set(cv2.CAP_PROP_POS_FRAMES, frame_count * fps)
+
+        success, im = vidcap.read()
+        if not success:
+            break
+
+        dets = detector.detect(im[:, :, ::-1])[:, :4]
+        faces_in_frame = extract_faces_from_image(im, dets)
+        all_faces.extend(faces_in_frame)
+        frame_count += 1
+
+    return all_faces
+
+
+# Streamlit UI
+st.title("Face Extraction from Video")
+st.write("Upload an MP4 video to extract faces from it.")
+
+uploaded_video = st.file_uploader("Choose a video...", type=["mp4"])
+
+if uploaded_video is not None:
+    tfile = tempfile.NamedTemporaryFile(delete=False)
+    tfile.write(uploaded_video.read())
+
+    st.video(tfile.name)
+
+    if st.button("Extract Faces"):
+        extracted_faces = process_video(tfile)
+
+        st.write("Extracted Faces:")
+        cols = st.columns (3)  # Create 3 columns for the gallery
+
+        for idx, face_img in enumerate(extracted_faces):
+            with cols[idx % 3]:  # Cycle through columns
+                st.image(face_img, channels="BGR", caption=f"Face #{idx + 1}")
